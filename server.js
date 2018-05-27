c267af29b44cf067411eeb59c0fa0ac76aaabe83
Steven Howard <steven@smhoward.net>
2018-05-26 23:27:05 -0400

    updated for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -79,7 +80,7 @@ app.get('/bad', (req, res) => {
     ]
   });
 });
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
 
